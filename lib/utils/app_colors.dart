9ad252bb8eedbe22ed3dc21f5893ca2429fc84bd
nada <n.1762002@gmail.com>
2025-07-03 16:50:17 +0300

    imported assets, created styles and colors, created adaptive layout

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+abstract final class AppColors {
+  static const Color darkBlue = Color(0xff064061);
+  static const Color blue = Color(0xff208CC8);
+  static const Color lightBlue = Color(0xff4EB7F2);
+  static const Color darkGrey = Color(0xffC4C4C4);
+  static const Color grey = Color(0xffAAAAAA);
+  static const Color lightGrey = Color(0xffE2DECD);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff292D32);
+  static const Color red = Color(0xffF3735E);
+  static const Color green = Color(0xff7DD97B);
+}
